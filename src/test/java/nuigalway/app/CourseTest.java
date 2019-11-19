55367716cec9ac3cbf231d4fdc636d2fa6add3ba
HaoLin <h.lin2@nuigalway.ie>
2019-11-19 16:23:22 +0000

    Commit 1

---
+++ src/test/java/nuigalway/app/CourseTest.java
@@ -0,0 +1,81 @@
+package nuigalway.app;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+
+import static org.mockito.Mockito.mock;
+
+public class CourseTest {
+
+    @Test
+    public void Test_Junit_1_add_remove_modules(){
+        DateTime start = new DateTime("2000-01-01");
+        DateTime end = new DateTime("2010-01-01");
+        Course_Programme uut = new Course_Programme("BP", start, end);
+
+        Module m1 = mock(Module.class);
+        Module m2 = mock(Module.class);
+        uut.add_module(m1);
+        uut.add_module(m2);
+
+        ArrayList<Module> expected = new ArrayList<Module>();
+        expected.add(m1);
+        expected.add(m2);
+
+        Assert.assertEquals(expected, uut.getList_of_modules());
+
+        expected.remove(m1);
+        uut.remove_module(m1);
+
+        Assert.assertEquals(expected, uut.getList_of_modules());
+    }
+    @Test
+    public void Test_Junit_2_add_remove_students(){
+        DateTime start = new DateTime("2000-01-01");
+        DateTime end = new DateTime("2010-01-01");
+        Course_Programme uut = new Course_Programme("BP", start, end);
+
+        Student s1 = mock(Student.class);
+        Student s2 = mock(Student.class);
+        uut.add_student(s1);
+        uut.add_student(s2);
+
+        ArrayList<Student> expected = new ArrayList<Student>();
+        expected.add(s1);
+        expected.add(s2);
+
+        Assert.assertEquals(expected, uut.getList_of_students());
+
+        expected.remove(s1);
+        uut.remove_student(s1);
+
+        Assert.assertEquals(expected, uut.getList_of_students());
+    }
+
+    @Test
+    public void Test_Junit_3_getters_setters()
+    {
+        DateTime start = new DateTime("2000-01-01");
+        DateTime end = new DateTime("2010-01-01");
+
+        Course_Programme uut = new Course_Programme("BP", start, end);
+
+        Assert.assertEquals(start, uut.getStart_date());
+        Assert.assertEquals(end, uut.getEnd_date());
+        Assert.assertEquals("BP", uut.getName());
+
+        DateTime start_2 = new DateTime("2001-01-01");
+        DateTime end_2 = new DateTime("2011-01-01");
+
+        uut.setStart_date(start_2);
+        uut.setEnd_date(end_2);
+        uut.setName("ECE");
+
+        Assert.assertEquals(start_2, uut.getStart_date());
+        Assert.assertEquals(end_2, uut.getEnd_date());
+        Assert.assertEquals("ECE", uut.getName());
+    }
+}
---
+++ src/test/java/nuigalway/app/ModuleTest.java
@@ -0,0 +1,80 @@
+package nuigalway.app;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ModuleTest {
+
+    @Test
+    public void Test_Junit_1_add_remove_students() {
+        String name = "Software Engineering";
+        String id = "CT417";
+
+        Student student1 = mock(Student.class);
+        Student student2 = mock(Student.class);
+        when(student1.getUsername()).thenReturn("a1");
+        when(student2.getUsername()).thenReturn("a2");
+
+        Module uut = new Module(name, id);
+
+        uut.addStudent(student1);
+        uut.addStudent(student2);
+
+        ArrayList<Student> expected = new ArrayList<Student>();
+        expected.add(student1);
+        expected.add(student2);
+
+        Assert.assertEquals(expected, uut.get_students());
+
+        uut.removeStudent("a1");
+        Assert.assertNotEquals(expected, uut.get_students());
+
+
+    }
+
+    @Test
+    public void Test_Junit_2_add_remove_courses() {
+        String name = "Software Engineering";
+        String id = "CT417";
+        Module uut = new Module(name, id);
+
+        Course_Programme c1 = mock(Course_Programme.class);
+        Course_Programme c2 = mock(Course_Programme.class);
+
+        uut.addAssociated_course(c1);
+        uut.addAssociated_course(c2);
+
+        ArrayList<Course_Programme> expected = new ArrayList();
+        expected.add(c1);
+        expected.add(c2);
+
+        Assert.assertEquals(expected, uut.getAssociated_course());
+
+        expected.remove(c1);
+        uut.removeAssociated_courses(c1);
+
+        Assert.assertEquals(expected, uut.getAssociated_course());
+    }
+
+    @Test
+    public void Test_Junit_3_getters_setters()
+    {
+        String name = "Software Engineering";
+        String id = "CT417";
+        Module uut = new Module(name, id);
+
+        Assert.assertEquals(uut.getId(), "CT417");
+        Assert.assertEquals(uut.getModule_name(), "Software Engineering");
+
+        uut.setId("new Id");
+        uut.setModule_name("new Name");
+
+        Assert.assertEquals(uut.getId(), "new Id");
+        Assert.assertEquals(uut.getModule_name(), "new Name");
+    }
+}
